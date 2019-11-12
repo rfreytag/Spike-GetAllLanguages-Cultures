df995d363ca74920c01fb644c95cb7fb86cee6d4
Richard Freytag <rfreytag@freytag.us>
2019-11-12 17:21:52 -0500

    Add basic .NET console files.
     Changes to be committed:
    	new file:   Program.cs
    	new file:   Spike-GetAllLanguages-Cultures.csproj
    	new file:   Spike-GetAllLanguages-Cultures.sln

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spike_GetAllLanguages_Cultures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
